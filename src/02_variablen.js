5788b81c679e8386ba56b0f5f34f27c45bff05e7
kevinvega8 <155719154+kevinvega8@users.noreply.github.com>
2024-01-09 11:59:12 +0000

    02 Variablen vs. Konstanten in JS

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -12,32 +12,43 @@
 
 /* Deklaration + Wertzuweisung I */
 
-let firstName; // Deklaration (Definition)
-console.log(firstName); // Ausgabe
-firstName = "Max"; // Wertzuweisung | assigment
-console.log(firstName); // Ausgabe
-let familyName = "Mütze"; // Deklaration + Wertzuweisung
-console.log("Hi, ich bin " + firstName + " " + familyName); // Ausgabe
+// let firstName; // Deklaration (Definition)
+// console.log(firstName); // Ausgabe
+// firstName = "Max"; // Wertzuweisung | assigment
+// console.log(firstName); // Ausgabe
+// let familyName = "Mütze"; // Deklaration + Wertzuweisung
+// console.log("Hi, ich bin " + firstName + " " + familyName); // Ausgabe
 
-/* 
-   Wertzuweisung II + Datentypen
-   JS ist eine untypisierte Sprache!
-*/
+// /* 
+//    Wertzuweisung II + Datentypen
+//    JS ist eine untypisierte Sprache!
+// */
+
+// let test; // Deklaration
+// console.log("Datentyp: " + typeof test); // Ausgabe
+// console.log("Inhalt: " + test); // Ausgabe
+// console.log("--------"); // Ausgabe
+// test = "hello"; // string
+// console.log("Datentyp: " + typeof test); // Ausgabe
+// console.log("Inhalt: " + test); // Ausgabe
+// console.log("--------"); // Ausgabe
+// test = 2; // number
+// console.log("Datentyp: " + typeof test); // Ausgabe
+// console.log("Inhalt: " + test); // Ausgabe
+// console.log("--------"); // Ausgabe
+// test = true // boolean
+// console.log("Datentyp: " + typeof test); // Ausgabe
+// console.log("Inhalt: " + test); // Ausgabe
+// console.log("--------"); // Ausgabe
+
+/* Variablen vs. Konstanten in JS */
 
 let test; // Deklaration
-console.log("Datentyp: " + typeof test); // Ausgabe
+test = "Hi"; // Wertzuweisung
+test = "Hello"; // Wertzuweisung
 console.log("Inhalt: " + test); // Ausgabe
-console.log("--------"); // Ausgabe
-test = "hello"; // string
-console.log("Datentyp: " + typeof test); // Ausgabe
-console.log("Inhalt: " + test); // Ausgabe
-console.log("--------"); // Ausgabe
-test = 2; // number
-console.log("Datentyp: " + typeof test); // Ausgabe
-console.log("Inhalt: " + test); // Ausgabe
-console.log("--------"); // Ausgabe
-test = true // boolean
-console.log("Datentyp: " + typeof test); // Ausgabe
-console.log("Inhalt: " + test); // Ausgabe
-console.log("--------"); // Ausgabe
+
+const test2 = "Hi!"; // Deklaration + Wertzuweisung 
+console.log("Inhalt: " + test2); // Ausgabe
+
 
