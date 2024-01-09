d903772763408747a798acbf1cda537646277a2d
kevinvega8 <155719154+kevinvega8@users.noreply.github.com>
2024-01-09 11:25:58 +0000

    02 Wertzuweisung II + Datentypen

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -18,3 +18,26 @@ firstName = "Max"; // Wertzuweisung | assigment
 console.log(firstName); // Ausgabe
 let familyName = "Mütze"; // Deklaration + Wertzuweisung
 console.log("Hi, ich bin " + firstName + " " + familyName); // Ausgabe
+
+/* 
+   Wertzuweisung II + Datentypen
+   JS ist eine untypisierte Sprache!
+*/
+
+let test; // Deklaration
+console.log("Datentyp: " + typeof test); // Ausgabe
+console.log("Inhalt: " + test); // Ausgabe
+console.log("--------"); // Ausgabe
+test = "hello"; // string
+console.log("Datentyp: " + typeof test); // Ausgabe
+console.log("Inhalt: " + test); // Ausgabe
+console.log("--------"); // Ausgabe
+test = 2; // number
+console.log("Datentyp: " + typeof test); // Ausgabe
+console.log("Inhalt: " + test); // Ausgabe
+console.log("--------"); // Ausgabe
+test = true // boolean
+console.log("Datentyp: " + typeof test); // Ausgabe
+console.log("Inhalt: " + test); // Ausgabe
+console.log("--------"); // Ausgabe
+
